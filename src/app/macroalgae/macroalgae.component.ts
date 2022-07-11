acedabd7392ad1174210053c5e920a99f86551a0
Andre Rolim Behr <arbehr@inf.ufrgs.br>
2022-07-11 15:22:46 +0000

    Search filter and table header sorting

--- src/app/macroalgae/macroalgae.component.ts
+++ src/app/macroalgae/macroalgae.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Macroalgae } from '../macroalgae';
 import { MACROALGAE } from '../mock-macroalgae';
-import { Sort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { FormControl } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { map, startWith, catchError } from 'rxjs/operators';
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 import { MacroalgaeService } from '../services/macroalgae.service';
 import { MatDialog } from '@angular/material/dialog';
 import { InfoDialogComponent } from '../shared/components/info-dialog/info-dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-macroalgae',
@@ -18,16 +20,25 @@ import { InfoDialogComponent } from '../shared/components/info-dialog/info-dialo
 export class MacroalgaeComponent implements OnInit {
 
   myControl = new FormControl();
-  options: Macroalgae[] = MACROALGAE;
   filteredOptions: Observable<Macroalgae[]>;
   macroalgae_list$: Observable<Macroalgae[]>;
   displayedColumns: string[] = ['id', 'specie', 'status'];
-  
-  //sortedData: Observable<Macroalgae[]>;
+  sortedData: Macroalgae[] = [];
+
+
+  announceSortChange(sortState: Sort) {
+    console.log(sortState.direction)
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 
   constructor(private router: Router, 
     private macroalgaeService: MacroalgaeService,
-    public dialog: MatDialog) {
+    public dialog: MatDialog,
+    private _liveAnnouncer: LiveAnnouncer) {
     this.macroalgae_list$ = this.macroalgaeService.list()
     .pipe(
       catchError(error => {
@@ -35,12 +46,11 @@ export class MacroalgaeComponent implements OnInit {
         return of([]);
       })
     );
-    
-    //this.sortedData = this.macroalgae_list.slice();
+    this.macroalgae_list$.subscribe((data) => (this.sortedData = data as Macroalgae[]));
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.options.slice())),
+      map(name => (name ? this._filter(name) : this.sortedData.slice())),
     );
   }
 
@@ -64,12 +74,33 @@ export class MacroalgaeComponent implements OnInit {
   private _filter(specie: string): Macroalgae[] {
     const filterValue = specie.toLowerCase();
 
-    return this.options.filter(option => option.specie.toLowerCase().includes(filterValue));
+    return this.sortedData.filter(option => option.specie.toLowerCase().includes(filterValue));
   }
   
   getDetails() {
     if(this.myControl.value.id) {
       this.router.navigate(['/detail/' + this.myControl.value.id])
     }
+  }
+
+  sortData(sort: Sort) {
+    const data = this.sortedData.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+    this.sortedData = data.sort((a: any, b: any) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return this.compare(a.id, b.id, isAsc);
+        case 'specie':
+          return this.compare(a.specie, b.specie, isAsc);
+        case 'status':
+          return this.compare(a.status, b.status, isAsc);
+        default:
+          return 0;
+      }
+    });
   }
 }
